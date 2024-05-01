d446fb635404f73294be1f5a2589547398983276
Wattana <wattama.po@ku.th>
2024-05-01 17:56:14 +0700

    sing

--- test.py
+++ test.py
@@ -1,15 +1,56 @@
-from picamera import PiCamera
-from time import sleep
+from flask import Flask, Response
+import cv2
 
-camera = PiCamera()
+app = Flask(__name__)
 
-# กำหนดค่าสำหรับการแสดงตัวอย่างให้เป็น YUV420
-camera.resolution = (960, 720)
-camera.framerate = 30
-camera.start_preview()
 
-# รอเป็นเวลา 5 วินาที
-sleep(5)
+def gstreamer_pipeline(
+    capture_width=3280,
+    capture_height=2464,
+    display_width=820,
+    display_height=616,
+    framerate=21,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
-# ปิดกล้อง
-camera.stop_preview()
+
+camera = cv2.VideoCapture(0)
+# camera = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1234)
